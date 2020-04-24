8cc090121796636749af1fd553e2ce8466796d5b
kangheewon <ymcho24@naver.com>
2020-04-25 01:57:21 +0900

    Lab1_Login_Relative

---
+++ app/src/main/java/com/example/lab1_login_relative/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lab1_login_relative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
