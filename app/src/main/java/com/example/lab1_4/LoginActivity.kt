fb2e996e59e9b22ed9121b1028adddf203466456
kangheewon <ymcho24@naver.com>
2020-05-07 05:16:13 +0900

    성장과제2(수정)

--- app/src/main/java/com/example/lab1_4/LoginActivity.kt
+++ app/src/main/java/com/example/lab1_4/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,13 +16,12 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val et_id = findViewById<EditText>(R.id.et_id)
-        val et_pwd = findViewById<EditText>(R.id.et_pwd)
-        val btn_login = findViewById<Button>(R.id.btn_login)
-        val tv_register = findViewById<TextView>(R.id.tv_register)
+        tv_register.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivityForResult(intent, 201)
+        }
 
-
-        val msg = App.prefs.myEditText
+        /*val msg = App.prefs.myEditText
         if (msg == "") { /*RegisterActivity에서 넣어준 값이 없거나 앱 설치한 초기 상태일 경우*/
 
         }
@@ -29,7 +29,18 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, "자동 로그인", Toast.LENGTH_LONG).show()
             val intent1 = Intent(this,MainActivity::class.java)
             startActivity(intent1) /*MainActivity로 이동*/
+        }*/
+
+        if (App.prefs.myEditText == "") { /*RegisterActivity에서 넣어준 값이 없거나 앱 설치한 초기 상태일 경우*/
+
         }
+        else{ /*prefs에 넣어준 값이 있으면*/
+            Toast.makeText(this, "자동 로그인", Toast.LENGTH_LONG).show()
+            val intent1 = Intent(this,MainActivity::class.java)
+            startActivity(intent1) /*MainActivity로 이동*/
+        }
+
+
 
         btn_login.setOnClickListener {
             if(et_id.text.isNullOrBlank()||et_pwd.text.isNullOrBlank()){
@@ -41,15 +52,13 @@ class LoginActivity : AppCompatActivity() {
                 위해 여기서 prefs값을 넣어줌. 만약 이 단계가 없으면 처음에 회원가입 단계 거치지 않을 경우 자동 로그인 x */
                 val intent = Intent(this,MainActivity::class.java)
                 startActivity(intent)
+                finish()
                 /* install하고 처음으로 회원가입 단계 없이 로그인 시도할 경우 '자동로그인' 토스트 안 뜸.
                 * 앱 종료하고 다시 들어가면 그때는 prefs 값이 있으므로 토스트 메세지 발생함. */
 
             }
         }
-        tv_register.setOnClickListener{
-            val intent = Intent(this,RegisterActivity::class.java)
-            startActivityForResult(intent, 201)
-        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,11 +66,15 @@ class LoginActivity : AppCompatActivity() {
 
         val id = data?.getStringExtra("id")
         val pass = data?.getStringExtra("pass")
-        val et_id = findViewById<EditText>(R.id.et_id)
-        val et_pwd = findViewById<EditText>(R.id.et_pwd)
 
         et_id.setText(id)
         et_pwd.setText(pass)
+
+        if ((resultCode == Activity.RESULT_OK) && (App.prefs.myEditText != "")) {
+            Toast.makeText(this, "자동 로그인", Toast.LENGTH_LONG).show()
+            val intent2 = Intent(this,MainActivity::class.java)
+            startActivity(intent2) /*MainActivity로 이동*/
+        }
 
     }
 
--- app/src/main/java/com/example/lab1_4/RegisterActivity.kt
+++ app/src/main/java/com/example/lab1_4/RegisterActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -14,20 +15,16 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val btn_join = findViewById<Button>(R.id.btn_join)
-        val ed1 = findViewById<EditText>(R.id.et_id1)
-
         btn_join.setOnClickListener{
 
-            App.prefs.myEditText = ed1.text.toString()  /* 회원가입 단계를 거쳤다는 것을 loginActivity에게
+            App.prefs.myEditText = et_id1.text.toString()  /* 회원가입 단계를 거쳤다는 것을 loginActivity에게
              알려주기 위해 prefs에 값을 넣어줌. 이 prefs 값은 앱이 종료되더라도 남아있음. */
 
             val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+            setResult(Activity.RESULT_OK, intent)
             finish()
 
         }
-
 
     }
 }
